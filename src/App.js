622a93eaf0e5bee733f5d1287c1362240487363d
Fitooohhh <avm0098@gmail.com>
2026-09-16 22:52:35 -0400

    feat: proyecto base

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Tarea 2.1 - Repositorios y Forks</h1>
+      <p>Proyecto Integrador</p>
+    </div>
+  );
+}
+
+export default App;
